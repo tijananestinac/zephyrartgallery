f7e957e154b3745f2324a88ee329db0207bc98e5
tijananestinac <51137771+tijananestinac@users.noreply.github.com>
2019-06-25 00:36:40 +0200

    Add files via upload

---
+++ js1/init.js
@@ -0,0 +1,25 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+     
+
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+  
+  $(document).ready(function(){
+    $('.modal').modal();
+	$('.modal').modal('methodName', paramName);
+
+  });
